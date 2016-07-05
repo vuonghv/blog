515f2b7d7f379cc8ad3901e04ca8fcc3579eba58
Vuong Hoang <vuonghv.cs@gmail.com>
2016-07-05 10:51:14 +0700

    Add user and post schema

---
+++ post.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    body: { type: String },
+    comments: [ { body: String, date: Date, author: String }],
+    insDate: { type: Date, default: Date.now },
+    isPublic: Boolean,
+    meta: {
+        votes: Number,
+        favs: Number
+    }
+});
+
+module.exports = postSchema;
+module.exports.set('toObject', { virtuals: true });
+module.exports.set('toJSON', { virtuals: true });
