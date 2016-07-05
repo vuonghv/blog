a2ebacef2c3a570959d3e81b18d3e3db9fb74265
Vuong Hoang <vuonghv.cs@gmail.com>
2016-07-05 15:50:06 +0700

    Implement GET articles blog API

--- api.js
+++ api.js
@@ -12,14 +12,57 @@ module.exports = function(wagner) {
         return function(req, res) {
             Post.create(req.body, function(err, doc) {
                 if (err) {
-                    return res.
-                        status(status.BAD_REQUEST).
-                        json({ error: err.toString() });
+                    return res
+                        .status(status.BAD_REQUEST)
+                        .json({ error: err.toString() });
                 }
-                return res.json(doc);
+                return res.status(status.CREATED).json(doc);
             });
+        };
+    }));
+
+    api.get('/posts/:id', wagner.invoke(function(Post) {
+        return function(req, res) {
+            Post.findOne({ _id: req.params.id }, function(err, doc) {
+                if (err) {
+                    return res
+                        .status(status.INTERNAL_SERVER_ERROR)
+                        .json({ error: err.toString() });
+                }
+
+                if (!doc) {
+                    return res
+                        .status(status.NOT_FOUND)
+                        .json({ error: 'Not found' });
+                }
+
+                res.json(doc);
+            });
+        };
+    }));
+
+    api.get('/posts', wagner.invoke(function(Post) {
+        return function(req, res) {
+            let sort = { insDate: -1 };
+
+            Post
+                .find({})
+                .sort(sort)
+                .exec(handleMany.bind(null, 'posts', res));
         };
     }));
 
     return api;
 };
+
+function handleMany(property, res, error, result) {
+    if (error) {
+        return res
+            .status(status.INTERNAL_SERVER_ERROR)
+            .json({ error: error.toString() });
+    }
+
+    let obj = {};
+    obj[property] = result;
+    res.json(obj);
+}
